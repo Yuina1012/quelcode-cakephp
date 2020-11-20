3cd5c156d69e0b0f37128fa826d028d65f08180d
小泉結以奈 <mizugoro1012@gmail.com>
2020-11-20 10:38:53 +0900

    bidinfo カラム追加 migration実行

---
+++ html/mycakeapp/config/Migrations/20201120013802_AddColumnsToBidinfo.php
@@ -0,0 +1,42 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddColumnsToBidinfo extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('bidinfo');
+        $table->addColumn('buyer_name', 'string', [
+            'default' => null,
+            'limit' => 100,
+            'null' => false,
+        ]);
+        $table->addColumn('buyer_address', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('buyer_tel', 'string', [
+            'default' => null,
+            'limit' => 13,
+            'null' => false,
+        ]);
+        $table->addColumn('status', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
