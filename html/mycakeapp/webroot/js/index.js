3b808e48d73ff41a802ae095792bc37cd78c1eed
小泉結以奈 <mizugoro1012@gmail.com>
2021-01-14 10:41:39 +0900

    let → const　に修正

--- html/mycakeapp/webroot/js/index.js
+++ html/mycakeapp/webroot/js/index.js
@@ -5,16 +5,16 @@ setInterval(()=> {
 		console.log(dif);
 		// 表記
 		const day = parseInt(dif / 60 / 60 / 24, 10); //日
-		let dif_hour = dif % 86400;
+		const dif_hour = dif % 86400;
 		console.log(dif_hour);
 		const hour = parseInt(dif_hour / 60 / 60, 10); //時
-		let dif_min = dif_hour % 3600;
+		const dif_min = dif_hour % 3600;
 		console.log(dif_min);
 		const min = parseInt(dif_min / 60, 10); //分
-		let dif_sec = dif_min % 60;
+		const dif_sec = dif_min % 60;
 		console.log(dif_sec);
 		const sec = parseInt(dif_sec, 10); //秒
-		let timer = day + '日' + hour + '時間' + min + '分' + sec + '秒';
+		const timer = day + '日' + hour + '時間' + min + '分' + sec + '秒';
 		console.log(dif_time);
 		console.log(day);
 		console.log(hour);
